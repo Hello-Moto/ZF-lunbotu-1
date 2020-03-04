c7aa2819410fe4aff5b7579d76d300b4e3c0d7f3
Hello-Moto <you@example.com>
2020-03-04 22:19:03 +0800

    渐显轮播

--- js/index.js
+++ js/index.js
@@ -1,121 +1,145 @@
-let bannerRender = (function(){
-	let banner = document.getElementById('banner'),
-		 wrapper = document.getElementById('wrapper'),
-		 focus = document.getElementById('focus'),
-		 prev = document.getElementById('prev'),
-		 next = document.getElementById('next'),
-		 list = null,
-		 divList = null,
-		 stepIndex = 0,
-		 autoTime = null,
-		 interval = 2000;
-	//获取数据
-	let queryData = function queryDate(){
-		return new Promise((resolve, reject) => {
-		let xhr = new XMLHttpRequest();
-			xhr.open('get','json/banner.json',true);
-			xhr.onreadystatechange = ()=>{
-				if(xhr.readyState === 4 && xhr.status === 200){
-					let data = JSON.parse(xhr.responseText);
-					resolve(data);
-				}
-			};
-			xhr.send(null);
-		});
-	};
-	//数据绑定
-	let bindHTML = function bindHTML(data){
-		let str = ``,
-			 strList = ``;
-		data.forEach((item,index)=>{
-			let {img='default.jpg',desc='无图'} = item;
-			str += `<div><img src="${img}" alt="${desc}"></div>`;
-			strList += `<li class="${index === 0 ? 'active' : null}"></li>`;
-		});
-		str += `<div><img src="${data[0].img}" alt="${data[0].desc}"></div>`;
-		wrapper.innerHTML = str;
-		focus.innerHTML = strList;
-		//重新获取div和li
-		divList = wrapper.querySelectorAll('div');
-		list = focus.querySelectorAll('li');
-		utils.css(wrapper,'width',divList.length * (utils.css(divList[0],'width')));
-	};
-
-	//自动切换
-	let autoMove = function autoMove(){
-		stepIndex++;
-		if(stepIndex > divList.length - 1){
-			utils.css(wrapper,'left',0);
-			stepIndex = 1;
-		}
-		animate(wrapper,{left: (utils.css(divList[0],'width'))*-stepIndex},300);
-		changeFocus();
-	};
-
-	//焦点按钮
-	let changeFocus = function changeFocus(){
-		let tempIndex = stepIndex;
-		tempIndex === divList.length -1 ? tempIndex = 0 : null;
-		[].forEach.call(list,(item,index)=>{
-			item.className = tempIndex === index ? 'active' : '';
-		})
-	};
-
-	//鼠标移入移出事件
-	let handleBanner = function handleBanner(){
-		banner.onmouseenter = function () {
-			clearInterval(autoTime);
-			next.style.display = prev.style.display = 'block';
-		};
-		banner.onmouseleave = function(){
-			autoTime = setInterval(autoMove,interval);
-			next.style.display = prev.style.display = 'none';
-		}
-	};
-
-	//焦点切换
-	let handleFocus = function handleFocus(){
-		[].forEach.call(list,(item,index)=>{
-			item.onclick = function () {
-				stepIndex = index;
-				animate(wrapper,{
-					left: (utils.css(divList[0],'width'))*-stepIndex,
-				},300);
-				changeFocus();
-			}
-		})
-	};
-
-	//给左右切换添加点击事件
-	let handleRL = function handleRL(){
-		next.onclick = autoMove;
-		prev.onclick = function(){
-			stepIndex--;
-			if(stepIndex < 0){
-				utils.css(wrapper,'left',-(divList.length - 1)*(utils.css(divList[0],'width')));
-				stepIndex = divList.length - 2;
-			}
-			animate(wrapper,{
-				left: (utils.css(divList[0],'width'))*-stepIndex,
-			},300);
-			changeFocus();
-		}
-	};
-
-	return {
-		init: function init() {
-			let promise = queryData();
-			promise.then(bindHTML).then(()=>{
-				autoTime = setInterval(autoMove,interval);
-				document.addEventListener('visibilitychange',()=>{
-					if(document.hidden){
-						clearInterval(autoTime)
-					}else{
-						autoTime = setInterval(autoMove,interval);
+$(function (){
+	let bannerRender = (function bannerRender(){
+		let $banner = $('#banner'),
+			 $wrapper = $('#wrapper'),
+			 $focus = $('#focus'),
+			 $next = $('#next'),
+			 $prev = $('#prev'),
+			 $focusList = null,
+			 $divList = null;
+		//获取数据
+		let queryDate = function queryDate(){
+			return new Promise((resolve, reject) => {
+				$.ajax({
+					url: './json/banner.json',
+					method: 'get',
+					dataType: 'json',
+					async: 'true',
+					success: (data) => {
+						resolve(data);
+					},
+					error: (msg) => {
+						reject(msg);
 					}
+					// success: resolve,
+					// error: reject,
 				});
-			}).then(handleBanner).then(handleFocus).then(handleRL )
+			})
+		};
+
+		//数据绑定
+		let bindHTML = function bindHTML(data){
+			let str = ``,
+				 focusListStr = ``;
+			$.each(data,(index,item)=>{
+				let {img,desc} = item;
+				str += `<div><img src="${img}" alt=""></div>`;
+				focusListStr += `<li class="${index === 0 ? 'active' : 'null'}"></li>`;
+			});
+			$wrapper.html(str);
+			$focus.html(focusListStr);
+			$divList = $wrapper.find('div');
+			$focusList = $focus.find('li');
+		};
+
+		//自动轮播
+		let _index = 0,
+			 _nextIndex = 0,
+			 _timer = null,
+			 _interval = 3000,
+			 _speed = 500,
+			 _move = false;
+
+		//优化
+		let changePicture = function changePicture(){
+			let $cur = $divList.eq(_nextIndex),
+				 $last = $divList.eq(_index);
+			$cur.css('zIndex',1);
+			$last.css('zIndex',0);
+			$cur.stop().animate({opacity: 1},_speed,()=>{
+				$last.css({opacity: 0});
+				_index = _nextIndex;
+				_move = false;
+			});
+			changeFocus();
+		};
+
+		let autoMove = function autoMove(){
+			_nextIndex++;
+			if(_nextIndex >= $divList.length){
+				_nextIndex = 0;
+			}
+			changePicture();
+		};
+
+		//焦点对齐
+		let changeFocus = function changeFocus(){
+			$focusList.eq(_nextIndex).addClass('active');
+			$focusList.eq(_index).removeClass('active');
+		};
+
+		 //鼠标移入
+		let handleMouse = function handleMouse(){
+			$banner.on('mouseenter',()=>{
+				clearInterval(_timer);
+				$('.banner .arrow').css('display','block');
+			});
+			$banner.on('mouseleave',()=>{
+				_timer = setInterval(autoMove,_interval);
+				$('.banner .arrow').css('display','none');
+			});
+		};
+
+		//左右切换
+		let changeRl = function changeRl(){
+			$next.on('click',()=>{
+				if(_move){
+					return;
+				}
+				_move = true;
+				autoMove();
+			});
+			$prev.on('click',()=>{
+				if(_move){
+					return;
+				}
+				_move = true;
+				_nextIndex--;
+				if(_nextIndex < 0){
+					_nextIndex = $divList.length - 1;
+				}
+				changePicture();
+			})
+		};
+
+		//焦点切换
+		let changeFocusRL = function changeFocus(){
+			$focusList.on('click',function (){
+				if(_move){
+					return;
+				}
+				_move = true;
+				let curIndex = $(this).index();
+				if(_nextIndex === curIndex){
+					return;
+				}
+				_nextIndex = curIndex;
+				changePicture();
+			})
+		};
+
+		return {
+			init: function init() {
+				let promise = queryDate();
+				promise.then((data)=>{
+					bindHTML(data);
+					_timer = setInterval(autoMove,_interval);
+					handleMouse();
+					changeRl();
+					changeFocusRL();
+				})
+			}
 		}
-	}
-})();
-bannerRender.init();
+	})();
+	bannerRender.init();
+});
